4864e21518fde3d82979d6c5ef7899cc9428618a
kkang2623 <kkagn2623@naver.com>
2022-10-25 21:10:56 +0900

    chapter1 - 출력

---
+++ src/main/java/j01_출력/PrintTest1.java
@@ -0,0 +1,25 @@
+package j01_출력;
+
+/**
+ *
+ */
+public class PrintTest1 {
+    public static void main(String[] args) { //main 안에 local 또는 지역 이라 부른다
+    //한 줄 주석
+    /*
+    여러 줄 주석
+     */
+        /**
+         * 클래스 또는 메소드의 기능을 설명하기 위한 주석
+         */
+
+        // 문자열 출력
+        System.out.print("이름: \n"); //여러 글자를 적을 때 큰 따음표 // 문자열 // \n : 줄 바꿈
+        System.out.print("남강석");
+        System.out.print('1'); //한 글자를 적을 때 작은 따음표      //
+        System.out.print("주소: "); //println 은 한줄로만 입력 \n  을 쓰지 않고도 줄 바꿈
+        System.out.println("부산시 해운대구");
+
+        boolean isUsernameCheckFlags = false; //
+    }
+}
